1a330006fde32c841e30e78b960b34dc6e6f2b8b
Thalles Carneiro <thallescarneiro@gmail.com>
2024-04-14 11:12:02 -0300

    feat(backend): adiciona interfaces de produto e pack

---
+++ backend/interfaces/pack.interface.ts
@@ -0,0 +1,8 @@
+interface Pack {
+  id: number;
+  pack_id: number;
+  product_id: number;
+  qty: number;
+}
+
+export default Pack;
---
+++ backend/interfaces/product.interface.ts
@@ -0,0 +1,8 @@
+interface Product {
+  code: number;
+  name: string;
+  cost_price: number;
+  sales_price: number;
+}
+
+export default Product;
